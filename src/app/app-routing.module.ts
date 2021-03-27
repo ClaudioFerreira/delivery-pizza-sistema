6266d11fcc12ac919768ae8c78ad1f64492de840
Claudio Ferreira <claudio.zero0@gmail.com>
2021-03-27 16:59:45 -0300

    feat(register) layout inicial da pagina de registro do usuário

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
+import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'entrar', component: LoginComponent}
+  { path: 'entrar', component: LoginComponent },
+  { path: 'cadastrar', component: RegisterComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
