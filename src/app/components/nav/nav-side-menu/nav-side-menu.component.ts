36c78fd7867d243d9b755a385f3582dc1738c377
Claudio Ferreira <claudio.zero0@gmail.com>
2021-04-01 19:18:10 -0300

    feat(header/footer/menu) layout inicial

---
+++ src/app/components/nav/nav-side-menu/nav-side-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-side-menu',
+  templateUrl: './nav-side-menu.component.html',
+  styleUrls: ['./nav-side-menu.component.scss']
+})
+export class NavSideMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
